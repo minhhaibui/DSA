775f0b51ebda7853225938c57498b7903d169337
Haibm <haibm@avadagroup.com>
2025-10-17 00:20:40 +0700

    two-dimensional array

--- src/DSA_Array/Two_dimensional_array.java
+++ src/DSA_Array/Two_dimensional_array.java
@@ -1,5 +1,34 @@
 package DSA_Array;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Two_dimensional_array {
+        public static void main(String[] args) {
+            Random rd = new Random();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("nhap n: ");
+            int n = sc.nextInt();
+            System.out.println("nhap m: ");
+            int m=sc.nextInt();
+
+            int [][] arr=new int[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                   arr[i][j]= rd.nextInt(1000);
+                }
+            }
+
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    System.out.print(arr[i][j]);
+                    if (j < m - 1) System.out.print(", ");
+                }
+                System.out.println();
+            }
+
+        }
 
 }
