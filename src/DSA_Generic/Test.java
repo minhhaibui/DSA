758b12c88ee72deb7a9455ee098f8c5cea90ad69
Haibm <haibm@avadagroup.com>
2025-10-30 00:03:36 +0700

    DSA đếm số

--- src/DSA_Generic/Test.java
+++ src/DSA_Generic/Test.java
@@ -1,18 +1,35 @@
 package DSA_Generic;
 
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        Dictionary <String,String> dictionary = new Dictionary<>("Hello","xin chao");
-        System.out.println(dictionary.toString());
-        Dictionary<String,String> [] arrDictionary = new Dictionary[20];
-        arrDictionary[0] =  new Dictionary<>("Hello","xin chao");
-        arrDictionary[1] =  new Dictionary<>("Hello","xin chao");
-        arrDictionary[2] =  new Dictionary<>("Hello","xin chao");
-        arrDictionary[3] =  new Dictionary<>("Hello","xin chao");
-        for (Dictionary<String, String> stringStringDictionary : arrDictionary) {
-            System.out.println(stringStringDictionary.toString());
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("nhap n");
+            int N = sc.nextInt();
+            System.out.println("nhap x");
+            int X =sc.nextInt() ;
+            int[] a = new int[N];
+            int count = 0;
+
+            for (int i = 0; i < N; i++) a[i] = sc.nextInt();
+
+            for (int i = 0; i < N; i++) System.out.print(a[i] + " ");
+
+            for (int i = 0; i < N; i++){
+                for (int j = i; j < N; j++){
+                    if(a[i] * a[i] + a[j] == X){
+                        count++;
+                    }
+                }
+            };
+
+
+            System.out.println(count);
         }
-    }
+
 }
